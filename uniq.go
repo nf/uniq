9020593c80bdeebcf32c20626b748cf75f1518e0
nf@wh3rd.net <nf@amchitka.local>
2010-08-29 11:59:30 +1000

    initial commit

---
+++ uniq.go
@@ -0,0 +1,26 @@
+package uniq
+
+import (
+    "sync"
+)
+
+var (
+    uniqLock sync.Mutex
+    uniqId int
+)
+
+func uniqFn() int {
+    uniqLock.Lock()
+    defer uniqLock.Unlock()
+    uniqId++
+    return uniqId
+}
+
+var uniq = make(chan int)
+
+func init() {
+    go func() {
+            for i := 1;; i++ { uniq <- i }
+    }()
+}
+
---
+++ uniq_test.go
@@ -0,0 +1,19 @@
+package uniq
+
+import (
+    "testing"
+)
+
+func BenchmarkMutex(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        uniqFn()
+    }
+}
+
+func BenchmarkChannel(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        <-uniq
+    }
+}
+
+func TestFoo(t *testing.T) { }
